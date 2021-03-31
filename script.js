5a660b8b681ee9eee3f57816710e7a024710cf06
wwwikla <50991164+wiklapandu@users.noreply.github.com>
2021-04-01 00:45:40 +0700

    mengubah tabel menjadi contact Us dan menambahkan github stats

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navbar = document.getElementsByClassName('navbar-nav')[0];
 const home = document.querySelectorAll('nav-link')[0];
-const navLink = document.getElementsByClassName('nav-link');
+const navLik = document.getElementById('nav-link');
 navbar.addEventListener('click', (e) => {
     for (let i = 0; i < navLink.length; i++) {
         navLink[i].classList.remove('active');
